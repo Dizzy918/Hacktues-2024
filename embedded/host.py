af7bd819a40dc6c920ff02105c6496851351bb8b
milar111 <114105495+milar111@users.noreply.github.com>
2024-03-14 14:21:15 +0200

    host
    host code for pc without the selenium part

---
+++ embedded/host.py
@@ -0,0 +1,51 @@
+import argparse
+import re
+import serial
+import time
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("port", type=str, help="Serial port of the board", nargs=1)
+args = parser.parse_args()
+port = args.port
+
+channel = serial.Serial(args.port[0])
+channel.timeout = 0.05
+
+def check_for_bracets(s):
+    pattern = r'^\((.*)\)$'
+    return bool(re.match(pattern, s))
+
+
+username = ""
+password = ""
+while True:
+    
+    line = None
+    try:
+        line = channel.readline()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt - quitting")
+        exit()
+    
+
+    if line:
+        try:
+            decoded_line = line.decode("utf8")
+            print(decoded_line)
+
+            if check_for_bracets(decoded_line):
+                if(len(username) == 0):
+                    username = decoded_line
+                else:
+                    password = decoded_line
+
+            if len(username) > 0 and len(password) > 0:
+            #call selenium
+            
+            # empty password and email
+        except ValueError:
+            print("error decode")
+            pass
+    
+    time.sleep(0.1)
