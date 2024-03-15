3d4251c781476732f9e7b820f5e82ede3604ef00
milar111 <114105495+milar111@users.noreply.github.com>
2024-03-16 00:42:41 +0200

    Animation
    In this code animations are changed if the button is pressed

--- piPicoWithAnimation.py
+++ piPicoWithAnimation.py
@@ -9,9 +9,8 @@ import displayio
 import adafruit_displayio_ssd1306
 import adafruit_imageload
 
-
-email = "email"
-password = "password"
+email = "daniel.b.yordanov.2022@elsys-bg.org"
+password = "dani08201214012119"
 
 email1 = f"({email})"
 password1 = f"({password})"
@@ -22,11 +21,11 @@ button_login.switch_to_input(pull = digitalio.Pull.UP)
 button_logout = digitalio.DigitalInOut(board.GP0)
 button_logout.switch_to_input(pull = digitalio.Pull.UP)
 
-
-
-IMAGE_FILE = "bee.bmp"
+IMAGE_FILE1 = "bee.bmp"
+IMAGE_FILE2 = "loading1.bmp"
 SPRITE_SIZE = (64, 64)
-FRAMES = 28
+FRAMES1 = 28
+FRAMES2 = 20
 
 def invert_colors(palette):
     palette[0], palette[1] = palette[1], palette[0]
@@ -39,30 +38,55 @@ display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 
 group = displayio.Group()
 
-icon_bit, icon_pal = adafruit_imageload.load(IMAGE_FILE, bitmap=displayio.Bitmap, palette=displayio.Palette)
-invert_colors(icon_pal)
+icon_bit1, icon_pal1 = adafruit_imageload.load(IMAGE_FILE1, bitmap=displayio.Bitmap, palette=displayio.Palette)
+icon_bit2, icon_pal2 = adafruit_imageload.load(IMAGE_FILE2, bitmap=displayio.Bitmap, palette=displayio.Palette)
+invert_colors(icon_pal1)
+invert_colors(icon_pal2)
 
-icon_grid = displayio.TileGrid(icon_bit, pixel_shader=icon_pal, width=1, height=1, tile_height=SPRITE_SIZE[1], tile_width=SPRITE_SIZE[0], default_tile=0, x=32, y=0)
-group.append(icon_grid)
+icon_grid1 = displayio.TileGrid(icon_bit1, pixel_shader=icon_pal1, width=1, height=1, tile_height=SPRITE_SIZE[1], tile_width=SPRITE_SIZE[0], default_tile=0, x=32, y=0)
+icon_grid2 = displayio.TileGrid(icon_bit2, pixel_shader=icon_pal2, width=1, height=1, tile_height=SPRITE_SIZE[1], tile_width=SPRITE_SIZE[0], default_tile=0, x=32, y=0)
+group.append(icon_grid1)
 
 display.show(group)
 
 timer = 0
-pointer = 0
-
-
+pointer1 = 0
+pointer2 = 0
 
 while True:
-    # 2 events
-    # Login -> "Login: (username, password)"
-    # Logout -> "Logout"
     if button_login.value == False:
+        print(email1)
+        print(password1)
+        if icon_grid1 in group:
+            group.remove(icon_grid1)
+        if icon_grid2 not in group:
+            group.append(icon_grid2)
+        for i in range(FRAMES2):
+            icon_grid2[0] = i
+            time.sleep(0.70)
         time.sleep(1)
+        if icon_grid2 in group:
+            group.remove(icon_grid2)
+        if icon_grid1 not in group:
+            group.append(icon_grid1)
     elif button_logout.value == False:
+        print("Logout")
+        if icon_grid1 in group:
+            group.remove(icon_grid1)
+        if icon_grid2 not in group:
+            group.append(icon_grid2)
+        for i in range(FRAMES2):
+            icon_grid2[0] = i
+            time.sleep(0.25) 
         time.sleep(1)
-
-    if (timer + 0.1) < time.monotonic():
-        icon_grid[0] = pointer
-        pointer = (pointer + 1) % FRAMES
-        timer = time.monotonic()
-
+        if icon_grid2 in group:
+            group.remove(icon_grid2)
+        if icon_grid1 not in group:
+            group.append(icon_grid1)
+    else:
+        if icon_grid2 in group:
+            group.remove(icon_grid2)
+        if icon_grid1 not in group:
+            group.append(icon_grid1)
+        icon_grid1[0] = (icon_grid1[0] + 1) % FRAMES1
+        time.sleep(0.1)
