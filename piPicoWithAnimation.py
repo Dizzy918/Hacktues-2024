92f73823c30cdcf9bd0a0e00bb4f612b32bca5be
milar111 <114105495+milar111@users.noreply.github.com>
2024-03-15 18:19:42 +0200

    buttons_switched

--- piPicoWithAnimation.py
+++ piPicoWithAnimation.py
@@ -10,16 +10,16 @@ import adafruit_displayio_ssd1306
 import adafruit_imageload
 
 
-email = "gmail"
-password = "password"
+email = "daniel.b.yordanov.2022@elsys-bg.org"
+password = "dani08201214012119"
 
 email1 = f"({email})"
 password1 = f"({password})"
 
-button_login = digitalio.DigitalInOut(board.GP0)
+button_login = digitalio.DigitalInOut(board.GP1)
 button_login.switch_to_input(pull = digitalio.Pull.UP)
 
-button_logout = digitalio.DigitalInOut(board.GP1)
+button_logout = digitalio.DigitalInOut(board.GP0)
 button_logout.switch_to_input(pull = digitalio.Pull.UP)
 
 
@@ -68,3 +68,4 @@ while True:
         icon_grid[0] = pointer
         pointer = (pointer + 1) % FRAMES
         timer = time.monotonic()
+
