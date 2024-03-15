9a5a6397575bf959c96d70921cedf951a9ba6fe4
Merdzhak <alex.merdzhanov@gmail.com>
2024-03-15 15:45:04 +0200

    3:44
    3:44
    15.03

--- mainfunction.py
+++ mainfunction.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 import time
 import threading
 
-mail = input("Enter your mail: ")
-password = input("Enter your password: ")
+mail = 'aleks.i.gerasimov.2022@elsys-bg.org'
+password = 'Sladoled'
 
 running = True
 
@@ -34,49 +36,50 @@ def login(mail, password):
 
 def unlimited_sleep():
     global running
-    #while True:
     sleep(5)
     logout(driver)
 
 def logout(driver):
 
-    #t.join()
-
     print("A")  
     global running
 
-    sleep(5)
+    sleep(3)
 
+    driver.implicitly_wait(10)
     profile_button = driver.find_element(By.CLASS_NAME, "gb_Fa")
     profile_button.click()
-    print("b")
-    sleep(5)
+    iframe = driver.find_element(By.XPATH, "//iframe[@name='account']")
+    print(iframe)
+    driver.switch_to.frame(iframe)
 
-    signout_button = driver.find_element(By.CLASS_NAME, "JWEMkf")
+    container = driver.find_element(By.CLASS_NAME, "JWEMkf")
+    links = container.find_elements(By.TAG_NAME,"a")
+    print(links)
+    signout_button = links[1]
     signout_button.click()
-    print("c")
-    sleep(2)
+    sleep(7)
 
-    removeprofile_button = driver.find_element(By.CLASS_NAME, "lCoei YZVTmd SmR8")
+    driver.switch_to.default_content()
+
+    container2 = driver.find_element(By.CLASS_NAME, "OVnw0d")
+    options = container2.find_elements(By.TAG_NAME, "li")
+    removeprofile_button = options[2]
     removeprofile_button.click()
-    sleep(2)
+    print("d")
+    sleep(7)
 
     redminus_button = driver.find_element(By.CLASS_NAME, "stUf5b")
     redminus_button.click()
-    sleep(2)
+    sleep(7)
 
-    logoutconfirmation_button = driver.find_element(By.CLASS_NAME, "ZFr60d CeoRYc")
+    logoutconfirmation_button = driver.find_element(By.CLASS_NAME, "RveJvd snByac")
     logoutconfirmation_button.click()
-    sleep(2)
+    sleep(3)
 
     running = False
     driver.quit()
 
 driver = login(mail, password)
 
-t = threading.Thread(target = unlimited_sleep())
-t.start()
-
 logout(driver)
-
-sleep(30)
