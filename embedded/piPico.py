ad3927a1c37a9858ff273dd3fa78baac584ab163
milar111 <114105495+milar111@users.noreply.github.com>
2024-03-15 12:23:32 +0200

    pico

--- embedded/piPico.py
+++ embedded/piPico.py
@@ -5,20 +5,30 @@ import microcontroller
 import random
 import usb_cdc
 
-email = "gmail.vom"
-password = "password"
+email = "daniel.b.yordanov.2022@elsys-bg.org"
+password = "dani08201214012119"
 
 email1 = f"({email})"
 password1 = f"({password})"
 
-button = digitalio.DigitalInOut(board.GP0)
-button.switch_to_input(pull = digitalio.Pull.UP)
+button_login = digitalio.DigitalInOut(board.GP0)
+button_login.switch_to_input(pull = digitalio.Pull.UP)
+
+button_logout = digitalio.DigitalInOut(board.GP1)
+button_logout.switch_to_input(pull = digitalio.Pull.UP)
 
 
 
 while True:
-    if button.value == False:
+    # 2 events
+    # Login -> "Login: (username, password)"
+    # Logout -> "Logout"
+    if button_login.value == False:
         print(email1)
         print(password1)
         time.sleep(1)
+    elif button_logout.value == False:
+        print("Logout")
+        time.sleep(1)
 
+        
